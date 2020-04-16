669fa7d77f673e0b5cfe4585a972fdb73e616cb2
aminahaid <aminahaid@gmail.com>
2020-04-16 16:05:30 -0500

    v commit

---
+++ src/declareVariable.java
@@ -0,0 +1,22 @@
+
+public class declareVariable {
+    public static void main(String[] args) {
+        String name = "MalI";
+        long million = 20000000L;
+        String capital = "Niarobi";
+        String currency = "shilling";
+        System.out.println("Hey Mom and Dad!" + "\n" + "Here is some interesting details about the first country i am visiting" + "\n"
+                + "Name: " + name + "\n" + "Population size: " + million  + " Million" + "\n" + "Capital: " + capital + "\n" + "Currency: One U.S Dollar equals $103.84 Kenyan Shillings!! "
+                + "\n" + "I am so excited to visit the Nairobi Nation Park and feel the giraffes!! Talk later.");
+    }
+}
+
+
+
+
+
+
+
+
+
+
